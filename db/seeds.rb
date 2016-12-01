65e7844b1aeb24a281ea7fc4dd20fe5963de0b37
chen hongji <chenhongji@chendeMBP.fios-router.home>
2016-11-30 20:03:16 -0500

    add new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,10 +57,58 @@ Favor.create(user_id: 3, recipe_id: 2)
 Favor.create(user_id: 4, recipe_id: 4)
 Favor.create(user_id: 5, recipe_id: 1)
 
-Ingredient.create(name: "beef", recipe_id: 1, amount: "50g")
-Ingredient.create(name: "powder", recipe_id: 1, amount: "50g")
-Ingredient.create(name: "vegetable", recipe_id: 1, amount: "50g")
+Ingredient.create(name: "apple", recipe_id: 1, amount: "50g")
+Ingredient.create(name: "banana", recipe_id: 1, amount: "50g")
+Ingredient.create(name: "mac", recipe_id: 1, amount: "50g")
 
-Step.create(recipe_id: 1, index: 1, content: "cook beef", picture: "uploads/832e.jpg")
-Step.create(recipe_id: 1, index: 2, content: "cook powder", picture: "uploads/832e.jpg")
-Step.create(recipe_id: 1, index: 3, content: "cook vegetable", picture: "uploads/832e.jpg")
+Ingredient.create(name: "apple", recipe_id: 2, amount: "50g")
+Ingredient.create(name: "banana", recipe_id: 2, amount: "50g")
+Ingredient.create(name: "mac", recipe_id: 2, amount: "50g")
+
+Ingredient.create(name: "apple", recipe_id: 3, amount: "50g")
+Ingredient.create(name: "banana", recipe_id: 3, amount: "50g")
+Ingredient.create(name: "mac", recipe_id: 3, amount: "50g")
+
+Ingredient.create(name: "apple", recipe_id: 4, amount: "50g")
+Ingredient.create(name: "banana", recipe_id: 4, amount: "50g")
+Ingredient.create(name: "mac", recipe_id: 4, amount: "50g")
+
+Ingredient.create(name: "apple", recipe_id: 5, amount: "50g")
+Ingredient.create(name: "banana", recipe_id: 5, amount: "50g")
+Ingredient.create(name: "mac", recipe_id: 5, amount: "50g")
+
+Ingredient.create(name: "apple", recipe_id: 6, amount: "50g")
+Ingredient.create(name: "banana", recipe_id: 6, amount: "50g")
+Ingredient.create(name: "mac", recipe_id: 6, amount: "50g")
+
+Ingredient.create(name: "apple", recipe_id: 7, amount: "50g")
+Ingredient.create(name: "banana", recipe_id: 7, amount: "50g")
+Ingredient.create(name: "mac", recipe_id: 7, amount: "50g")
+
+Step.create(recipe_id: 1, index: 1, content: "recipe 1 step 1", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 1, index: 2, content: "recipe 1 step 2", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 1, index: 3, content: "recipe 1 step 3", picture: "uploads/832e.jpg")
+
+Step.create(recipe_id: 2, index: 1, content: "recipe 2 step 1", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 2, index: 2, content: "recipe 2 step 2", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 2, index: 3, content: "recipe 2 step 3", picture: "uploads/832e.jpg")
+
+Step.create(recipe_id: 3, index: 1, content: "recipe 3 step 1", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 3, index: 2, content: "recipe 3 step 2", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 3, index: 3, content: "recipe 3 step 3", picture: "uploads/832e.jpg")
+
+Step.create(recipe_id: 4, index: 1, content: "recipe 3 step 1", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 4, index: 2, content: "recipe 3 step 2", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 4, index: 3, content: "recipe 3 step 3", picture: "uploads/832e.jpg")
+
+Step.create(recipe_id: 5, index: 1, content: "recipe 3 step 1", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 5, index: 2, content: "recipe 3 step 2", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 5, index: 3, content: "recipe 3 step 3", picture: "uploads/832e.jpg")
+
+Step.create(recipe_id: 6, index: 1, content: "recipe 3 step 1", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 6, index: 2, content: "recipe 3 step 2", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 6, index: 3, content: "recipe 3 step 3", picture: "uploads/832e.jpg")
+
+Step.create(recipe_id: 7, index: 1, content: "recipe 3 step 1", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 7, index: 2, content: "recipe 3 step 2", picture: "uploads/832e.jpg")
+Step.create(recipe_id: 7, index: 3, content: "recipe 3 step 3", picture: "uploads/832e.jpg")
